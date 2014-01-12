5200e78065dca8e828738e93be67a68b41bbd581
justingrammens <justin@localtone.com>
2014-01-12 07:42:20 -0600

    latest commit

---
+++ arduino/latest_motion_arduino/latest_motion_arduino.ino
@@ -0,0 +1,156 @@
+
+/*
+  Web client
+ 
+ This sketch connects to a website (http://www.google.com)
+ using a WiFi shield.
+ 
+ This example is written for a network using WPA encryption. For 
+ WEP or WPA, change the Wifi.begin() call accordingly.
+ 
+ This example is written for a network using WPA encryption. For 
+ WEP or WPA, change the Wifi.begin() call accordingly.
+ 
+ Circuit:
+ * WiFi shield attached
+ 
+ created 13 July 2010
+ by dlf (Metodo2 srl)
+ modified 31 May 2012
+ by Tom Igoe
+ */
+
+
+#include <SPI.h>
+#include <WiFi.h>
+#include <Time.h>
+
+char ssid[] = "PEKE"; //  your network SSID (name) 
+char pass[] = "jandb2005";    // your network password (use for WPA, or use as key for WEP)
+
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+int pirPin = 2; //digital 2
+boolean motion_state = false;
+
+boolean testing = true;
+
+int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,192,141);  // numeric IP for Google (no DNS)
+char server[] = "localtone-gae.appspot.com";    // name address for Google (using DNS)
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server 
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
+int counter = 0;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600); 
+ pinMode(pirPin, INPUT);
+     
+ while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+ }
+  
+  Serial.println("Attempting to connect to WPA network...");
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present"); 
+    // don't continue:
+    //while(true);
+  } 
+  
+  status = WiFi.begin(ssid, pass);
+  delay(15000);
+   
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present"); 
+    // don't continue:
+    while(true);
+  } 
+  
+  if ( status != WL_CONNECTED) { 
+    Serial.println("Couldn't get a wifi connection");
+    // don't do anything else:
+    while(true);
+  } 
+  else {
+    
+    printWifiStatus();
+  }
+  
+}
+
+void loop() {
+  
+  int pirVal = digitalRead(pirPin);
+
+  if(pirVal == LOW){ //was motion detected
+    Serial.println("Motion Detected"); 
+      postData("motion");
+      delay(1000);
+      
+  } else {
+    Serial.println("no motion detected");
+  }
+  delay(2000);
+}
+
+void postData(String motion) {
+  
+   if (client.connect(server, 80)) {
+      Serial.println("connected");
+      // Make a HTTP request:
+      // Make a HTTP request:
+        client.println("PUT /events HTTP/1.1");
+        client.println("Host: localtone-gae.appspot.com");
+        client.println("User-Agent: Arduino/1.0");
+        client.println("Connection: close");
+        client.println("Content-Type: application/json");
+        client.print("Content-Length:");
+        
+        //String dataj = "<Activity><DeviceID>HARDWARE44</DeviceID><Kind>" + motion + "</Kind></Activity>";
+        String dataj = "{\"DeviceID\": \"HARDWAREJSONTRUEREAL\", \"Kind\": \"" + motion + "\"}";
+        
+        client.println(dataj.length());
+        client.println();
+        client.println(dataj);
+     
+      Serial.println("Done posting!!!" + dataj);
+      
+    } else {
+      Serial.println("Not able to connect!!!!!!");
+    }
+      
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+
+
+
+
--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-		"html/template"
 		"appengine"
 		"appengine/datastore"
         "github.com/emicklei/go-restful"
@@ -9,18 +8,8 @@ import (
         "net/http"
 		"log"
 		"time"
+		"github.com/kevinburke/twilio-go/twilio"
 )
-
-// This example is functionally the same as ../restful-user-service.go
-// but it`s supposed to run on Goole App Engine (GAE)
-//
-// contributed by ivanhawkes
-
-type Greeting struct {
-    Author  string
-    Content string
-    Date    time.Time
-}
 
 
 type Activity struct {
@@ -28,14 +17,6 @@ type Activity struct {
 	Kind string
 	Date    time.Time
 }
-
-/*type ActivityDevice struct {
-    DeviceID  string
-    Kind string
-    Date    time.Time
-}*/
-
-
 
 type EventService struct {
         // normally one would use DAO (data access object)
@@ -56,70 +37,15 @@ func (u EventService) Register() {
                 Reads(Activity{})) // from the request
 
 
-/*        ws.Route(ws.GET("/{user-id}").To(u.findEvent).
-                // docs
-                Doc("get a user").
-                Param(ws.PathParameter("user-id", "identifier of the user").DataType("string")).
-                Writes(User{})) // on the response
-*/
         ws.Route(ws.PUT("/{user-id}").To(u.createEvent).
                 // docs
                 Doc("create an event").
                 Param(ws.PathParameter("user-id", "identifier of the user").DataType("string")).
                 Reads(Activity{})) // from the request
-				/*
-        ws.Route(ws.DELETE("/{user-id}").To(u.removeEvent).
-                // docs
-                Doc("delete a user").
-                Param(ws.PathParameter("user-id", "identifier of the user").DataType("string")))
-*/
+				
         restful.Add(ws)
 }
 
-// GET http://localhost:8080/users/1
-//
-/*
-func (u EventService) findEvent(request *restful.Request, response *restful.Response) {
-        c := appengine.NewContext(request.Request)
-        id := request.PathParameter("user-id")
-        usr := new(User)
-		
-		
-        _, err := memcache.Gob.Get(c, id, &usr)
-        if err != nil || len(usr.Id) == 0 {
-                response.WriteErrorString(http.StatusNotFound, "User could not be found.")
-        } else {
-                response.WriteEntity(usr)
-        }
-}
-
-// PATCH http://localhost:8080/users
-// <User><Id>1</Id><Name>Melissa Raspberry</Name></User>
-//
-func (u *UserService) updateEvent(request *restful.Request, response *restful.Response) {
-        c := appengine.NewContext(request.Request)
-        evt := new(Event)
-        err := request.ReadEntity(&evt)
-        if err == nil {
-                item := &memcache.Item{
-                        Key:    evt.Id,
-                        Object: &usr,
-                }
-                err = memcache.Gob.Set(c, item)
-                if err != nil {
-                        response.WriteError(http.StatusInternalServerError, err)
-                        return
-                }
-                response.WriteEntity(usr)
-        } else {
-                response.WriteError(http.StatusInternalServerError, err)
-        }
-}
-
-// PUT http://localhost:8080/users/1
-// <User><Id>1</Id><Name>Melissa</Name></User>
-//
-*/
 
 // guestbookKey returns the key used for all guestbook entries.
 func eventKey(c appengine.Context) *datastore.Key {
@@ -128,50 +54,38 @@ func eventKey(c appengine.Context) *datastore.Key {
 	return datastore.NewKey(c, "Collection", "default_stuff", 0, nil)
 }
 
+func sendText() {
+	const sid = "AC3ce9c81dec2c21f0664f44a2effb604e"
+    const token  = "bcea6eac803c01755daa1968ac10caa5"
+	
+	client := twilio.CreateClient(sid, token, nil)
+	msg, err := client.Messages.SendMessage("+16122604503", "+16122088663", "Movement happened!", nil)
+	
+	log.Printf("MESSAGE IS: %s", msg)
+	log.Printf("err is: %s", err)
+}
+
 func (u *EventService) createEvent(request *restful.Request, response *restful.Response) {
         c := appengine.NewContext(request.Request)
 		
-		
-//	    event := new(Greeting) ??? WTF this didn't work? but below did...
 	    event := Activity{}
 	    request.ReadEntity(&event)	
 		
 		log.Printf("HERE IS TYPER!!!")
 	
-		 // persist the event
-/*		event := Event{
-		        Id: time.Now() ,
-		        Type: typer,
-		}
-*/		
 		key := datastore.NewIncompleteKey(c, "Activity", eventKey(c))
 		_, err2 := datastore.Put(c, key, &event)
   
 	    if err2 == nil {
 			response.WriteHeader(http.StatusCreated)
 	        response.WriteEntity(event)
+			
+			sendText()
+			
 	    } else {
 	        response.WriteError(http.StatusInternalServerError,err2)
 	    }
-			
-        //usr := User{Id: request.PathParameter("user-id")}
-        //err := request.ReadEntity(&usr)
-        /*if err == nil {
-                item := &memcache.Item{
-                        Key:    usr.Id,
-                        Object: &usr,
-                }
-                err = memcache.Gob.Add(c, item)
-                if err != nil {
-                        response.WriteError(http.StatusInternalServerError, err)
-                        return
-                }
-                response.WriteHeader(http.StatusCreated)
-                response.WriteEntity(usr)
-        } else {
-                response.WriteError(http.StatusInternalServerError, err)
-        }*/
-}
+	}
 
 
 // GET http://localhost:8080/users
@@ -194,65 +108,12 @@ func (u *EventService) getAllEvents(request *restful.Request, response *restful.
 		        http.Error(response, err.Error(), http.StatusInternalServerError)
 		        return
 		    }
-			
-		    /*if err := guestbookTemplate.Execute(response, events); err != nil {
-		        http.Error(response, err.Error(), http.StatusInternalServerError)
-		    }*/
-			
-			/*
-		    if _, err := q.GetAll(c, &events); err != nil {
-		        http.Error(response, err.Error(), http.StatusInternalServerError)
-		        return
-		    }
-			*/
-			//log.Printf("HERE ARE EVENTS!!!! %s", events)
-			
+					
 			log.Printf("GOT HERE!")
 			response.WriteEntity(events)
-						
-			
-/*		    if err := guestbookTemplate.Execute(w, greetings); err != nil {
-		        http.Error(w, err.Error(), http.StatusInternalServerError)
-		    }
-			*/
-			
+									
 }
 
-
-var guestbookTemplate = template.Must(template.New("book").Parse(guestbookTemplateHTML))
-
-const guestbookTemplateHTML = `
-<html>
-<body>
-    {{range .}}
-      {{with .Kind}}
-        <p><b>{{.}}</b> wrote:</p>
-      {{else}}
-        <p>An anonymous person wrote:</p>
-      {{end}}
-      <pre>{{.Date}}</pre>
-    {{end}}
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-`
-
-
-// DELETE http://localhost:8080/users/1
-//
-/*
-func (u *UserService) removeUser(request *restful.Request, response *restful.Response) {
-        c := appengine.NewContext(request.Request)
-        id := request.PathParameter("user-id")
-        err := memcache.Delete(c, id)
-        if err != nil {
-                response.WriteError(http.StatusInternalServerError, err)
-        }
-}
-*/
 
 func getGaeURL() string {
         if appengine.IsDevAppServer() {
