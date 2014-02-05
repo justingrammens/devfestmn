8c54b29903eb233f02be2ed09340eb75aa175684
Justin Grammens <justin@code42.com>
2014-02-04 23:45:40 -0600

    adding multi return value function

--- mydate/mydate.go
+++ mydate/mydate.go
@@ -3,6 +3,12 @@ package mydate
 import "time"
 
 func Birthdate() time.Time {
-	d := time.Date(2014, time.May, 6, 0, 0, 0, 0, time.UTC)	// calling the time.Date function
+	d := time.Date(2014, time.May, 6, 0, 0, 0, 0, time.UTC) // calling the time.Date function
 	return d
+}
+
+func addsub(x, y int) (sum, difference int) {
+	sum = x + y
+	difference = x - y
+	return
 }
